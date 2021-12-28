b9e5e5ced8b8d1f4ba385f53d4adb52b35fdd16b
HASNAINMUBARAK <hasnainmubarak075@gmail.com>
2021-12-28 15:24:10 +0500

    account component complete

--- src/Components/Account/index.js
+++ src/Components/Account/index.js
@@ -1,71 +1,27 @@
 import React from "react";
 import Grid from "@material-ui/core/Grid";
-import Button from "@material-ui/core/Button";
 import Image from "../../Assets/Images/img2.png";
 import { Container } from "@material-ui/core";
 import "./style.css";
 
 const Account = () => {
   return (
-    <Container maxWidth="md" className="contain">
-      <Grid
-        container
-        item
-        // xs={12}
-        // md={12}
-        className="contain"
-        justifyContent="space-between"
-      >
-        <Grid item md={6} sm={6} className="left">
-          <div className="external">
-            <div className="upper">
-              <div className="user">
-                {/* <img className="userimg" src={Image1} alt="bro" /> */}
-                <p>@ain.ethrm</p>
-              </div>
-              <h2>Blob extrude with lines</h2>
-            </div>
-
-            <Grid item xs={12} className="timing-auction">
-              <Grid item>
-                <div className="box">
-                  <p className="bid">Current Bid</p>
-                  <p className="lbrt">1.18LBRT</p>
-                  <p className="dollar">26,000$</p>
-                </div>
-              </Grid>
-              {/* <Grid item>
-                <div className="horline">
-                  <hr className="hrr" />
-                </div>
-              </Grid> */}
-              <Grid className="timing">
-                <div className="box">
-                  <p className="bid">Auction ending in</p>
-                  <p className="min">11 40 30</p>
-                  <p className="dollar">Hours Min Seconds</p>
-                </div>
-              </Grid>
-            </Grid>
-
-            <div className="lower">
-              <div className="btns">
-                <Button className="bid" variant="contained">
-                  Place a bid
-                </Button>
-                <Button className="view" variant="contained">
-                  View artwork
-                </Button>
-              </div>
-            </div>
+    <Container className="contain">
+      <Grid container className="contain1">
+        <Grid item md={6} sm={6} xs={12} className="left">
+          <h1>WELCOME TO THE</h1>
+          <h1 className="silicon">Siliconbets</h1>
+          <p className="NFT">The easiest and most secure NFT marketpalce </p>
+          <div className="btns">
+            <button className="explore">Explore</button>
+            <button className="mint">Mint</button>
           </div>
         </Grid>
-        <Grid item md={6} sm={6} className="right">
+        <Grid item md={6} sm={6} xs={12} className="right">
           <img className="dummy" src={Image} alt="broo" />
         </Grid>
       </Grid>
     </Container>
   );
 };
-
 export default Account;
