ea25ff500d701c5413a868f822db61d36294adec
HASNAINMUBARAK <hasnainmubarak075@gmail.com>
2021-12-28 14:38:15 +0500

    Navbar component complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import "./App.css";
 import Navbar from "./Components/Navbar";
+import Account from "./Components/Account";
+
 function App() {
   return (
     <>
       <Navbar />
+      <Account />
     </>
   );
 }
--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -1,18 +1,40 @@
 import React from "react";
 import Grid from "@material-ui/core/Grid";
+import SearchIcon from "@material-ui/icons/Search";
+import Logo from "../../Assets/Images/Logo.png";
 import "./style.css";
 const Navbar = () => {
   return (
     <div>
-      <Grid container>
-        <Grid item md={6} xs={8}>
-          <h1>Hello world</h1>
+      <Grid container className="nav" spacing={3}>
+        <Grid item md={3} xs={6}>
+          <img src={Logo} alt=" " className="logo" />
         </Grid>
-        <Grid item md={6} xs={4}>
-          <ul>
-            <li>home</li>
-            <li>about</li>
-          </ul>
+        <Grid item md={2} xs={6}>
+          <div className="search-div">
+            <input placeholder="Search items collections" className="search" />
+            <SearchIcon className="search-icon" />
+          </div>
+        </Grid>
+        <Grid item md={5} xs={12} className="point">
+          <div className="four">
+            <ul>
+              <li>Explore</li>
+              <li>My profile</li>
+              <li>following</li>
+              <li>market</li>
+            </ul>{" "}
+          </div>
+          <div className="two">
+            <ul>
+              <li>how its work</li>
+              <li>Community</li>
+            </ul>{" "}
+          </div>
+        </Grid>
+        <Grid item md={2} xs={12} className="btns">
+          <button className="btn1">Create</button>
+          <button className="btn2">Sign In</button>
         </Grid>
       </Grid>
     </div>
