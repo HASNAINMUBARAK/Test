be85c826f993455c5a2a4d92697779af04f4261e
HASNAINMUBARAK <hasnainmubarak075@gmail.com>
2021-12-28 20:05:11 +0500

    task completed

--- src/Pages/Bidpage/index.js
+++ src/Pages/Bidpage/index.js
@@ -1,8 +1,12 @@
-import React from "react"
+import React from "react";
+import "./style.css";
+import { Link } from "react-router-dom";
 const Bidpage = () => {
   return (
     <>
-    <Link to="/home"><button>Back</button></Link>
+      <Link to="/">
+        <button className="btn">Back</button>
+      </Link>
       <h1>hello world</h1>
     </>
   );
